ce8a23f2bb649650dc5dbdb995a9378247ac5e1b
Pablo-Rodriguez <rodpepablo@gmail.com>
2020-07-03 12:05:52 +0200

    ADD: getTrackedEntityEvents

--- config/endpoints.js
+++ config/endpoints.js
@@ -13,6 +13,7 @@ module.exports = {
     GET_TYPES: () => '/trackedEntityTypes'
   },
   EVENTS: {
+    GET_TRACKED_ENTITY_EVENTS: () => '/events',
     GET_REPORTS: () => '/eventReports'
   },
   PROGRAMS: {
